d66d5d5a9bda9fb32ca3b4e971b7bb1b1cd5f4d0
Alexis Javier Levano Saire <levanosaire@gmail.com>
2024-07-27 18:18:22 -0500

    services updated

--- src/app/catalogue.service.ts
+++ src/app/catalogue.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CatalogueService {
-  public host: string = 'http://localhost:8080';
+  public host: string = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
   public getResource(url: string) { 
